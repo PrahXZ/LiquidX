b19e06e29b1ad4635f3ec07d0b7ae1c5b99af69a
PrahXZ <110250327+PrahXZ@users.noreply.github.com>
2022-12-27 14:20:11 -0500

    Best Librecraft Speed

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/Librecraft.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/movement/speeds/other/Librecraft.kt
@@ -1,11 +1,23 @@
 package net.ccbluex.liquidbounce.features.module.modules.movement.speeds.verus
 
 import net.ccbluex.liquidbounce.event.MoveEvent
+import net.ccbluex.liquidbounce.event.PacketEvent
+import net.ccbluex.liquidbounce.event.UpdateEvent
 import net.ccbluex.liquidbounce.features.module.modules.movement.speeds.SpeedMode
+import net.ccbluex.liquidbounce.features.value.BoolValue
+import net.ccbluex.liquidbounce.utils.MinecraftInstance
+import net.ccbluex.liquidbounce.utils.MovementUtils
 import net.ccbluex.liquidbounce.utils.MovementUtils.isMoving
 import net.ccbluex.liquidbounce.utils.MovementUtils.strafe
+import net.minecraft.network.Packet
+import net.minecraft.network.play.INetHandlerPlayServer
+import net.minecraft.network.play.client.*
+import net.minecraft.network.play.server.S08PacketPlayerPosLook
+
 
 class Librecraft : SpeedMode("Librecraft") {
+
+    private val timerBoostValue = BoolValue("TimerBoost", false)
     fun onMotion() {}
     override fun onUpdate() {
         if (!mc.thePlayer.isInWeb && !mc.thePlayer.isInLava && !mc.thePlayer.isInWater && !mc.thePlayer.isOnLadder && mc.thePlayer.ridingEntity == null) {
@@ -14,6 +26,13 @@ class Librecraft : SpeedMode("Librecraft") {
                 if (mc.thePlayer.onGround) {
                     mc.thePlayer.jump()
                     strafe(0.48f)
+                    if (timerBoostValue.get()) {
+                        if(mc.thePlayer.ticksExisted % 25 < 10) {
+                            mc.timer.timerSpeed = 2.25f
+                        } else {
+                            mc.timer.timerSpeed = 1.5f
+                        }
+                }
                 }
                 strafe()
             }
