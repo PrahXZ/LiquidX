70945ddd758b409837040267154275fc0e52d981
PrahXZ <rokero1890@gmail.com>
2023-01-07 09:26:34 -0500

    Renamed all

--- src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AutoLogin.kt
+++ src/main/java/net/ccbluex/liquidbounce/features/module/modules/misc/AutoLogin.kt
@@ -1,8 +1,4 @@
-/*
- * FDPClient Hacked Client
- * A free open source mixin-based injection hacked client for Minecraft using Minecraft Forge by LiquidBounce.
- * https://github.com/SkidderMC/FDPClient/
- */
+// LiquidX Development by PrahXZ and Haflin with FDP Base modified. v2.0 R1
 package net.ccbluex.liquidbounce.features.module.modules.misc
 
 import net.ccbluex.liquidbounce.event.EventTarget
